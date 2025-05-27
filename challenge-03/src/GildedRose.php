9134023061eb5c96371d42406cef6492172667e7
Cesar <cesarjcontreras30@gmail.com>
2025-05-26 23:02:16 -0500

    challenge 3: backstage passes

--- challenge-03/src/GildedRose.php
+++ challenge-03/src/GildedRose.php
@@ -68,6 +68,23 @@ class SulfurasItem extends BasicItem
     }
 }
 
+class BackstagePassesItem extends BasicItem
+{
+    public function tick()
+    {
+        $this->increaseQuality();
+        if ($this->sellIn < 6) {
+            $this->increaseQuality(2);
+        } elseif ($this->sellIn < 11) {
+            $this->increaseQuality();
+        }
+        $this->decreaseSellIn();
+        if ($this->sellIn < 0) {
+            $this->quality = 0;
+        }
+    }
+}
+
 class BasicItemFactory
 {
     public static function create($name, $quality, $sellIn): BasicItem
@@ -79,6 +96,8 @@ class BasicItemFactory
                 return new BrieItem($quality, $sellIn);
             case 'Sulfuras, Hand of Ragnaros':
                 return new SulfurasItem($quality, $sellIn);
+            case 'Backstage passes to a TAFKAL80ETC concert':
+                return new BackstagePassesItem($quality, $sellIn);
             default:
                 throw new \InvalidArgumentException("Unknown item type: $name");
         }
