a707f43da438415031b99ae1ef0b529d67a2e42e
Cesar <cesarjcontreras30@gmail.com>
2025-05-26 22:33:20 -0500

    challenge 3: normal item implemented

--- challenge-03/src/GildedRose.php
+++ challenge-03/src/GildedRose.php
@@ -1,6 +1,67 @@
 <?php
 
 namespace App;
+interface IBasicItem
+{
+    public function tick();
+}
+
+abstract class BasicItem implements IBasicItem
+{
+    public $name;
+
+    public $quality;
+
+    public $sellIn;
+
+    public function __construct($name, $quality, $sellIn)
+    {
+        $this->name = $name;
+        $this->quality = $quality;
+        $this->sellIn = $sellIn;
+    }
+    public function increaseQuality($quantity = 1)
+    {
+        if ($this->quality < 50) {
+            $this->quality += $quantity;
+        }
+    }
+    public function decreaseQuantity($quantity = 1)
+    {
+        if ($this->quality > 0) {
+            $this->quality -= $quantity;
+        }
+    }
+    public function decreaseSellIn()
+    {
+        $this->sellIn--;
+    }
+}
+
+class NormalItem extends BasicItem
+{
+    public function tick()
+    {
+        $this->decreaseSellIn();
+        $this->decreaseQuantity();
+        if ($this->sellIn < 0) {
+            $this->decreaseQuantity();
+        }
+    }
+}
+
+class BasicItemFactory
+{
+    public static function create($name, $quality, $sellIn): BasicItem
+    {
+        switch ($name) {
+            case 'normal':
+                return new NormalItem($name, $quality, $sellIn);
+            default:
+                throw new \InvalidArgumentException("Unknown item type: $name");
+        }
+    }
+}
 
 class GildedRose
 {
@@ -17,57 +78,16 @@ class GildedRose
         $this->sellIn = $sellIn;
     }
 
-    public static function of($name, $quality, $sellIn) {
+    public static function of($name, $quality, $sellIn)
+    {
         return new static($name, $quality, $sellIn);
     }
 
     public function tick()
     {
-        if ($this->name != 'Aged Brie' and $this->name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if ($this->quality > 0) {
-                if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-                    $this->quality = $this->quality - 1;
-                }
-            }
-        } else {
-            if ($this->quality < 50) {
-                $this->quality = $this->quality + 1;
-
-                if ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($this->sellIn < 11) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
-                    }
-                    if ($this->sellIn < 6) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-            $this->sellIn = $this->sellIn - 1;
-        }
-
-        if ($this->sellIn < 0) {
-            if ($this->name != 'Aged Brie') {
-                if ($this->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($this->quality > 0) {
-                        if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-                            $this->quality = $this->quality - 1;
-                        }
-                    }
-                } else {
-                    $this->quality = $this->quality - $this->quality;
-                }
-            } else {
-                if ($this->quality < 50) {
-                    $this->quality = $this->quality + 1;
-                }
-            }
-        }
+        $item = BasicItemFactory::create($this->name, $this->quality, $this->sellIn);
+        $item->tick();
+        $this->quality = $item->quality;
+        $this->sellIn = $item->sellIn;
     }
 }
